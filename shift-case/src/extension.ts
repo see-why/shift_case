698cd342f40504afbcf5ece5ad2acc579c449c16
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-03 23:46:18 -0400

    Refactor command implementation to toggle case of selected text in the editor

--- shift-case/src/extension.ts
+++ shift-case/src/extension.ts
@@ -13,14 +13,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('shift-case.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('shift-case.toggleCase', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from shift_case!');
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+			const text = document.getText(selection);
+
+			let newText: string;
+			if (text === text.toUpperCase()) {
+				newText = text.toLowerCase();
+			} else {
+				newText = text.toUpperCase();
+			}
+
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, newText);
+			});
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
